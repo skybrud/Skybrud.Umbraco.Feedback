5867b5dfb35c191eea9bedba3b5086db3e808758
Anders Bjerner <abjerner@skybrud.dk>
2022-02-08 10:10:57 +0100

    Entire "Skybrud.Feedback" folder doesn't appear to be used - probably a left over from the Umbraco 7 package

--- src/Skybrud.Umbraco.Feedback/App_Plugins/Skybrud.Feedback/Directives/loader.js
+++
@@ -1,7 +0,0 @@
-﻿angular.module("umbraco").directive('loader', function () {
-    return {
-        restrict: 'EA',
-        replace: true,
-        template: '<div class="loader"><div class="spinner"><div class="spinner-container container1"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div><div class="spinner-container container2"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div><div class="spinner-container container3"><div class="circle1"></div><div class="circle2"></div><div class="circle3"></div><div class="circle4"></div></div></div></div>'
-    };
-});
